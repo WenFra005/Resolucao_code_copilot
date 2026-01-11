36c59faf26a406b762b2880c1586fc331273e574
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-01-11 17:45:28 -0300

    feat: adiciona o arquivo concat_dados.py para concatenar informações do usuário

---
+++ concat_dados.py
@@ -0,0 +1,8 @@
+# Vamos receber dois dados diferentes do usuário e concatená-los em uma única string.
+
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+resultado = info1 + " " + info2
+
+print("Informações concatenadas:", resultado)
